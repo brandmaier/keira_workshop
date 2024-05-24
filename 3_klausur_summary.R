511e0c8978e813304c6861c6492daa7bddfd4dc7
Andreas Brandmaier <andy@brandmaier.de>
2024-05-24 08:45:50 +0200

    added nops-eval-fake

--- 3_klausur_summary.R
+++ 3_klausur_summary.R
@@ -1,7 +1,7 @@
 # Die Item Schwierigkeit lässt sich mit folgendem Befehl ausgeben:
 
-keira::plot_item_difficulty()
+keira::plot_item_difficulty(eval_file = "nops_eval_fake.csv")
 
 # Die Verteilung aller erreichten Punktzahlen lässt sich so darstellen
 
-keira::plot_points_distribution()
+keira::plot_points_distribution(eval_file = "nops_eval_fake.csv")
